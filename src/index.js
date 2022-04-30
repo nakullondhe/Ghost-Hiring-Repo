2efd80fc29d89a81469d2cdf8d428049e984741c
nakullondhe <nakulprivate27@gmail.com>
2022-04-30 16:55:06 +0530

    Databse added

--- src/index.js
+++ src/index.js
@@ -6,9 +6,17 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 8000;
 const path = require('path');
 
+const DB = "mongodb+srv://nik:1234@cluster0.g2sin.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongoose.connect(DB).then(() => {
+    console.log("DB connected")
+})
+
 app.listen(port, () => {
     console.log('Server started on port ' + port);
 })
+
+app.use(express.json());
 
 app.use('/',express.static(path.join(__dirname, 'public')));
 
